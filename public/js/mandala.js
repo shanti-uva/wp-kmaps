b53f5cb175338dd6b696d9e66ec6ae0d69f1ea8c
ThanGrove <than@virginia.edu>
2023-06-14 16:02:20 -0400

    moving scroll to top to work only if click is in same window

--- public/js/mandala.js
+++ public/js/mandala.js
@@ -60,7 +60,6 @@
     // Function to add "mandala" class to body when a link with a mandala hash is clickec
     // This has the effect of immediately hiding the WP content before the mandala content loads
     $('body').on('click', 'a', function (e) {
-        console.log("event in body a click", e);
         const ael = $(this);
         const anchor_ref = ael.data('anchor-ref');
         const href = ael.attr('href');
@@ -68,9 +67,8 @@
             const he = window?.mandala?.hash_execptions; // hash exceptions are set in the admin page
             const hash = '#/' + href.split("#/")[1];
             if (!he || !he.includes(hash)) {
-                console.log('event in mandala.js', e);
-                window.scrollTo(0, 0);
                 if (!$('body').hasClass('mandala') && e.originalEvent.which === 1 && e.originalEvent.metaKey === false) {
+                    window.scrollTo(0, 0);
                     $('body').addClass('mandala');
                 }
             }
